6513b2c6a6c6c1dfabf4c11aefe644977b1bdb4b
v0303v <volkov95@inbox.ru>
2023-11-23 17:16:06 +0500

    insert/update currencies

--- app/src/CurrencyHandler/CurrencyParser.php
+++ app/src/CurrencyHandler/CurrencyParser.php
@@ -3,10 +3,13 @@
 namespace app\CurrencyHandler;
 
 use app\Database\DatabaseConnection;
+use DateTime;
+use DateTimeZone;
 
 class CurrencyParser
 {
     private $db;
+    private $datetime;
 
     public function __construct()
     {
@@ -31,23 +34,31 @@ class CurrencyParser
      * Реализация сохранения данных в базу данных
      * @param $data
      * @return void
+     * @throws \Exception
      */
     private function saveCurrencyRatesToDatabase($data)
     {
-        foreach ($data as $currency) {
-            $query = $this->db->prepare("INSERT INTO currencies 
-                            (cbr_id, num_code, char_code, nominal, name, value, previous) 
-                            VALUES (?, ?, ?, ?, ?, ?, ?)");
+        $this->datetime = new DateTime('now', new DateTimeZone('Asia/Tashkent'));
+        $date = $this->datetime->format('Y-m-d H:i:s');
+        $query = $this->db->prepare("INSERT INTO currencies 
+                            (id, num_code, char_code, nominal, name, value, previous, timestamp) 
+                            VALUES (:id, :num_code, :char_code, :nominal, :name, :value, :previous, :timestamp)
+                            ON DUPLICATE KEY UPDATE 
+                            num_code = :num_code, char_code = :char_code, 
+                            nominal = :nominal, name = :name, 
+                            value = :value, previous = :previous, timestamp = :timestamp");
 
-            $query->execute([
-                $currency['ID'],
-                $currency['NumCode'],
-                $currency['CharCode'],
-                $currency['Nominal'],
-                $currency['Name'],
-                $currency['Value'],
-                $currency['Previous']
-            ]);
+        foreach ($data as $currency) {
+            $query->bindParam(':id', $currency['ID']);
+            $query->bindParam(':num_code', $currency['NumCode']);
+            $query->bindParam(':char_code', $currency['CharCode']);
+            $query->bindParam(':nominal', $currency['Nominal']);
+            $query->bindParam(':name', $currency['Name']);
+            $query->bindParam(':value', $currency['Value']);
+            $query->bindParam(':previous', $currency['Previous']);
+            $query->bindParam(':timestamp', $date);
+
+            $query->execute();
         }
     }
 }
--- databaseSqls/currencies.sql
+++ databaseSqls/currencies.sql
@@ -1,12 +1,11 @@
 CREATE TABLE currencies
 (
-    id        INT AUTO_INCREMENT PRIMARY KEY,
-    cbr_id    VARCHAR(10),
+    id        VARCHAR(10) PRIMARY KEY,
     num_code  VARCHAR(3),
     char_code VARCHAR(3),
     nominal   INT,
     name      VARCHAR(255),
     value     DECIMAL(10, 4),
     previous  DECIMAL(10, 4),
-    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    timestamp VARCHAR(255)
 );
